ddf85dbd8fdc40cc2b8e6b7012291f0574d23f79
hicham <rocky@localhost.localdomain>
2023-12-05 22:36:06 +0100

    AddTeamLeader guard

---
+++ src/auth/team-leader/team-leader.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtPayload } from '../jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UserType } from '@prisma/client';
+
+@Injectable()
+export class TeamLeaderGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) { }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+
+    const cookieToken = request.cookies.token;
+    if (!cookieToken) return false;
+
+
+    try {
+      const decoded = this.jwtService.verify(cookieToken, {
+        secret: process.env.JWT_SECRET
+      }) as JwtPayload;
+      request.user = decoded;
+      return decoded.userType === UserType.teamLeader ? true : false;
+    } catch (error) {
+      return false;
+    }
+  }
+}
